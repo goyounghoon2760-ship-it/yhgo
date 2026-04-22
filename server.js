766155d55a1cae3e998d60e9941620576f82596b
Future Party <test@futureparty.com>
2026-04-22 13:26:19 +0900

    fix: explicit Vercel config for views folder

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8080;
 
 // Set up EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(process.cwd(), 'views'));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
